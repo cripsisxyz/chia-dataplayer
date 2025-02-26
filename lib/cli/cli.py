f125ff519848f3f54c15754005a0b369385ca24d
Iván Vázquez <33484708+cripsisxyz@users.noreply.github.com>
2025-02-26 10:53:18 +0100

    Update cli.py

--- lib/cli/cli.py
+++ lib/cli/cli.py
@@ -1,32 +1,30 @@
 import logging, argparse, sys, yaml
 class Cli():
-    
+
     @staticmethod
     def set_app_logger():
         root = logging.getLogger()
         root.propagate = True
-        root.setLevel(eval(f"logging.{config['log_level']}"))
         handler = logging.StreamHandler(sys.stdout)
-        handler.setLevel(eval(f"logging.{config['log_level']}"))
         formatter = logging.Formatter('[%(levelname)s]|%(asctime)s|%(funcName)s|%(message)s')
         for handler in root.handlers:
             if isinstance(handler, logging.StreamHandler):
                 handler.setFormatter(formatter)
         handler.setFormatter(formatter)
         root.addHandler(handler)
-        
+
     @staticmethod
     def argparse():
         global args_global
-        
+
         def printText(args):
             print(args)
-            
+
         parser = argparse.ArgumentParser(description='Interact with Chia DataLayer in a easy way')
 
         parser.add_argument('-f', '--config-file', type=str, help='YAML config file. If not specified use default config.yaml', required=False)
         parser.add_argument('-m', '--mode', type=str, choices=['check', 'execute'], default='execute', required=False, help='Check only mode or really execute actions (default execute)')
-        
+
         subparser = parser.add_subparsers()
         datastores = subparser.add_parser('datastore')
         datastores.add_argument('--id', '-i', type=str, help='Datastore ID', required=True)
@@ -38,20 +36,20 @@ class Cli():
         datastores.add_argument('--value-to-file', '-vtf', type=str, help='Write value to a file path for read_key action if specified specified (optional)', required=False)
         datastores.add_argument('--cose-ph', '-cph', type=str, help='List of protected headers to store for update_key action and cose codec (optional). Format: FOO=BAR ABC=XYZ', nargs='+', required=False)
         datastores.add_argument('--cose-uh', '-cuh', type=str, help='List of unprotected headers to store for update_key action and cose codec (optional). Format: FOO=BAR ABC=XYZ', nargs='+', required=False)
-        
+
         datastores.set_defaults(func=printText)
         args_global = parser.parse_args()
         args_datastores, unknown = datastores.parse_known_args()
-        
+
         if args_global.mode == "check" and args_datastores.id is None:
             parser.print_help()
             sys.exit(0)
-            
+
         if args_datastores.action == "update_key":
             if args_datastores.key is None or (args_datastores.value is None and args_datastores.value_from_file is None):
                 logging.error("Key or value not specified for update_key action")
                 exit(1)
-                  
+
             if args_datastores.codec == "cose":
                 if args_datastores.cose_ph is not None:
                     try:
@@ -66,7 +64,7 @@ class Cli():
                         args_datastores.cose_ph = cose_ph
                 else:
                     args_datastores.cose_ph = {}
-                        
+
                 if args_datastores.cose_uh is not None:
                     try:
                         cose_uh = dict()
@@ -85,7 +83,7 @@ class Cli():
 
     def load_config_file():
         global config
-        
+
         if args_global.config_file is not None:
             filepath = args_global.config_file
         else:
