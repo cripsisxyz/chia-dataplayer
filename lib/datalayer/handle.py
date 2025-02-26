d63bc9ccc314681c1e3554be982d1eff10cb78db
Iván Vázquez <33484708+cripsisxyz@users.noreply.github.com>
2025-02-26 10:38:00 +0100

    fixes

--- lib/datalayer/handle.py
+++ lib/datalayer/handle.py
@@ -2,7 +2,7 @@ import os, json, logging
 from lib.datalayer.serial import Serializer
 
 class Handler():
-    
+
     def __init__(self, rpc_datalayer_instance):
         if os.getenv('COSE_KEY') is None:
             logging.warning("COSE_KEY environment variable not defined, USING A DEFAULT KEY for COSE ciphering")
@@ -10,7 +10,7 @@ class Handler():
         else:
             logging.info("COSE_KEY environment variable defined, using it")
             cose_key = os.getenv('COSE_KEY')
-            
+
         self.serial = Serializer(cose_key=cose_key)
         self.rpc_instance = rpc_datalayer_instance
 
@@ -30,11 +30,11 @@ class Handler():
         else:
             return(json.dumps(key_value, sort_keys=True, indent=2))
 
-    def hex_push_data(self, store_id=str, store_key=str, msg_store_value=str):
+    def hex_push_data(self, store_id: str, store_key: str, msg_store_value: str):
         encoded_message = self.serial.hex_encode(message=msg_store_value)
         rpc_changelist = [{"action":"insert", "key": self.serial.hex_encode(store_key), "value": encoded_message}]
         return(json.dumps(self.rpc_instance.datalayer_update_owned_store(store_id=store_id, change_list=rpc_changelist), sort_keys=True, indent=2))
-    
+
     def hex_read_data(self, store_id=str, store_key=str):
         key_value = self.rpc_instance.datalayer_get_value(store_id=store_id, key=self.serial.hex_encode(message=store_key))
         if not key_value:
@@ -42,7 +42,7 @@ class Handler():
         key_value_hex = self.serial.hex_decode(message=key_value['value'])
         key_value['value'] = key_value_hex
         return(json.dumps(key_value, sort_keys=True, indent=2))
-    
+
     def datastore_list_keys(self, store_id=str):
         keys = self.rpc_instance.datalayer_get_keys(store_id=store_id)
         keys_ascii = []
@@ -64,7 +64,7 @@ class Handler():
         with open(filepath, mode="rb") as rawfile:
             b64_file = self.serial.base64_bencode(message=rawfile.read())
         return(b64_file)
-    
+
     def write_local_file(self, filepath=str, filecontent=str):
         with open(filepath, mode="wb") as rawfile:
             rawfile.write(self.serial.base64_bdecode(filecontent))
